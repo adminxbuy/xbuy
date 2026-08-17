2698e0a514b3fd31458ff22e30c80d2a40814144
Marketplace Administrator <x-buy@admin.in>
2026-08-17 11:42:33 +0530

    style: increase footer top padding to pt-24

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Global Footer Component (Mercari Style, X-Buy Yellow Color) -->
-<footer class="bg-[#fdd835] text-zinc-900 mt-auto pt-16 pb-8 border-none">
+<footer class="bg-[#fdd835] text-zinc-900 mt-auto pt-24 pb-12 border-none">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         
         <!-- Top Section: Links & Accents -->
